368ca233f8aba7585c9596d8ae75b5ba878054e1
jottplus <jfisch.r92@gmail.com>
2021-01-02 18:17:51 +0100

    scrollable content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let rollerOpen = true;
+let last_known_scroll_position = 0;
+let ticking = false;
+
 (function() {
     resize();
     document.getElementById('handle-wrapper').onclick = function(e) {
@@ -8,9 +11,27 @@ let rollerOpen = true;
         if (!rollerOpen) openRoller();
     };
     window.addEventListener('resize', resize);
+
+    document.addEventListener('scroll', function(e) {
+        last_known_scroll_position = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                doSomething(last_known_scroll_position);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+
 })();
 
-function closeRoller() {            
+function doSomething(scroll_pos) {
+    if (rollerOpen && scroll_pos > 0) closeRoller();
+}
+
+function closeRoller() {
+    window.scrollTo(0, 0);         
     document.getElementById('thread').classList.remove('thread-swing');
     document.getElementById('handle').classList.remove('swing');
     document.getElementById('handle-wrapper').classList.remove('handle-open');
@@ -18,13 +39,13 @@ function closeRoller() {
     document.getElementById('roller-blind').classList.remove('roller-open');
     document.getElementById('roller-blind').classList.add('roller-close');
     setTimeout(function() {
-        document.getElementById('content').style.visibility = 'visible';
+        document.getElementById('content').style.visibility = 'visible';  
     }, 100);
     document
     rollerOpen = false;
 }
 
-function openRoller() {            
+function openRoller() {        
     document.getElementById('thread').classList.add('thread-swing');
     document.getElementById('handle').classList.add('swing');
     document.getElementById('handle-wrapper').classList.remove('handle-close');
@@ -32,12 +53,14 @@ function openRoller() {
     document.getElementById('roller-blind').classList.remove('roller-close');
     document.getElementById('roller-blind').classList.add('roller-open');
     setTimeout(function() {
-        document.getElementById('content').style.visibility = 'hidden';
+        document.getElementById('content').style.visibility = 'hidden'; 
     }, 300);
     rollerOpen = true;
 }
 
 function resize() {
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    document.getElementsByTagName('body')[0].style.height = vh + 'px';
     document.getElementById('content-image-wrapper').style.height = document.getElementById('content-image-wrapper').offsetWidth + 'px';
     document.getElementById('handle').style.height = document.getElementById('handle').offsetWidth + 'px';
 }
